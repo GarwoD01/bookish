0b4c2dd40f200f8ed0db8a5e5e828232a83186c1
Daniel Garwood <daniel.garwood@bbc.co.uk>
2022-10-12 18:48:59 +0100

    updated userController, added more fields to arrays

--- bookish-js-es6-mssql-starter/src/controllers/bookController.js
+++ bookish-js-es6-mssql-starter/src/controllers/bookController.js
@@ -4,20 +4,21 @@ import express from 'express';
 class BookController {
     constructor() {
         this.router = express.Router();
-        this.router.get('/', (request, response) => this.getAllBooks(request, response) );
-        this.router.get('/:id', (request, response) => this.getBook(request, response) );
-        this.router.post('/', (request, response) => this.postBook(request, response) );
-        this.router.delete('/:id', (request, response) => this.deleteBook(request, response) );
+        this.router.get('/', (request, response) => this.getAllBooks(request, response));
+        this.router.get('/:id', (request, response) => this.getBook(request, response));
+        this.router.post('/', (request, response) => this.postBook(request, response));
+        this.router.delete('/:id', (request, response) => this.deleteBook(request, response));
 
-        this.mockArray = [{'id' : 1, 'title' : 'Brave New World', 'isbn' : '9780582060166'}, 
-        {'id' : 2, 'title' : 'The Myth of Sisyphus', 'isbn' : '9780679733737'},
-        {'id' : 3, 'title' : 'Life 3.0','isbn' : '9783548377964'},
-        {'id' : 4, 'title' : 'Life 4.0','isbn' : '9783548377965'},
-        {'id' : 5, 'title' : 'Life 5.0','isbn' : '9783548377966'},
-        {'id' : 6, 'title' : 'Life 6.0','isbn' : '9783548377967'}];
+        this.booksArray = [{ 'id': 1, 'author' : 'Aldous Huxley', 'title': 'Brave New World', 'isbn': '9780582060166' },
+        { 'id': 2, 'author' : 'Albert Camus', 'title': 'The Myth of Sisyphus', 'isbn': '9780679733737' },
+        { 'id': 3, 'author' : 'Max Tegmark', 'title': 'Life 3.0', 'isbn': '9783548377964' },
+        { 'id': 4, 'author' : 'Max Tegmark', 'title': 'Life 4.0', 'isbn': '9783548377965' },
+        { 'id': 5, 'author' : 'Max Tegmark', 'title': 'Life 5.0', 'isbn': '9783548377966' },
+        { 'id': 6, 'author' : 'Max Tegmark', 'title': 'Life 6.0', 'isbn': '9783548377967' }];
 
         this.newBook = {
             "id": 7,
+            'author' : 'Jane Austen', 
             "title": "Pride and Prejudice",
             "isbn": "9783548377989"
         };
@@ -25,56 +26,45 @@ class BookController {
     }
 
     getAllBooks(request, response) {
-        console.log( "request for all books" + request.url );
-        for (let i = 0; i < this.mockArray.length; i++){
-            if ( undefined in this.mockArray[i]){
+        console.log("request for all books" + request.url);
+        /*for (let i = 0; i < this.booksArray.length; i++) {
+            if (undefined in this.booksArray[i]) {
 
             }
-            else{
-                this.newArray.push(this.mockArray[i]);
+            else {
+                this.newArray.push(this.booksArray[i]);
             }
-        }
-        response.status(200).send(this.newArray);
+        }*/
+        response.status(200).send(this.booksArray);
         // array filter
     }
 
     getBook(request, response) {
         const id = request.params.id;
-        console.log( "request for book " + id );
-        if ( id == 0 ){
-            throw ( "bad id");
+        console.log("request for book " + id);
+        if (id == 0) {
+            throw ("bad id");
         }
-        response.status(200).send(this.mockArray[id-1]);
+        response.status(200).send(this.booksArray[id - 1]);
     }
 
     postBook(request, response) {
-        console.log( "Posting new book" + request.url );
-        this.mockArray.push(this.newBook);
-        response.status(200).send(this.mockArray);
+        console.log("Posting new book" + request.url);
+        this.booksArray.push(this.newBook);
+        response.status(200).send(this.booksArray);
     }
 
     deleteBook(request, response) {
         const id = request.params.id;
-        console.log( "Delete book" + id);
-        delete this.mockArray[id-1];
-        response.status(200).send(this.mockArray);
+        console.log("Delete book" + id);
+        delete this.booksArray[id - 1];
+        response.status(200).send(this.booksArray);
 
         // array.map
         // .join
         // .find
         // .reduce
         // . filter
-    }
-
-    printBooks(mockArray,response){
-        for (let i = 0; i < mockArray.length; i++){
-            if ( null in mockArray.title){
-
-            }
-            else{
-                response.status(200).send(mockArray[i]);
-            }
-        }
     }
 }
 
--- bookish-js-es6-mssql-starter/src/controllers/userController.js
+++ bookish-js-es6-mssql-starter/src/controllers/userController.js
@@ -4,25 +4,68 @@ import express from 'express';
 class UserController {
     constructor() {
         this.router = express.Router();
-        this.router.get('/', (request, response) => this.getAllUsers(request, response) );
-        this.router.get('/:id', (request, response) => this.getUser(request, response) );
+        this.router.get('/', (request, response) => this.getAllUsers(request, response));
+        this.router.get('/:id', (request, response) => this.getUser(request, response));
+        this.router.post('/', (request, response) => this.postUser(request, response));
+        this.router.delete('/:id', (request, response) => this.deleteUser(request, response));
+
+        this.usersArray = [{ 'id': 1, 'name': 'Daniel Garwood', 'library_card': '05820601' },
+        { 'id': 2, 'name': 'Joshua Caddy', 'library_card': '06797337' },
+        { 'id': 3, 'name': 'Dave Artus', 'library_card': '35483779' },
+        { 'id': 4, 'name': 'Aaron Ossei', 'library_card': '35483779' },
+        { 'id': 5, 'name': 'Tom Porter', 'library_card': '35483779' },
+        { 'id': 6, 'name': 'Fabricio Melo', 'library_card': '35483779' }];
+
+        this.newUser = {
+            "id": 7,
+            "name": "Adam Bryan",
+            "library_card": "35483779"
+        };
+
+        this.newArray = [];
     }
 
     getAllUsers(request, response) {
-        console.log( "request for all users" + request.url );
-        response.status(200).send({"message" : "all users"});
+        console.log("request for all users" + request.url);
+        /*for (let i = 0; i < this.usersArray.length; i++) {
+            if (undefined in this.usersArray[i]) {
+
+            }
+            else {
+                this.newArray.push(this.usersArray[i]);
+            }
+        }*/
+        response.status(200).send(this.usersArray);
+        // array filter
     }
 
     getUser(request, response) {
         const id = request.params.id;
-        console.log( "request for user " + id );
-        if ( id == 0 ){
-            throw ( "bad id");
+        console.log("request for user " + id);
+        if (id == 0) {
+            throw ("bad id");
         }
-        const mock = {'id' : id, 'user' : 'user'};
-        response.status(200).send(JSON.stringify(mock));
+        response.status(200).send(this.usersArray[id - 1]);
+    }
+
+    postUser(request, response) {
+        console.log("Posting new user" + request.url);
+        this.usersArray.push(this.newUser);
+        response.status(200).send(this.usersArray);
+    }
+
+    deleteUser(request, response) {
+        const id = request.params.id;
+        console.log("Delete user" + id);
+        delete this.usersArray[id - 1];
+        response.status(200).send(this.usersArray);
+
+        // array.map
+        // .join
+        // .find
+        // .reduce
+        // . filter
     }
 }
 
 export default new UserController().router;
-
